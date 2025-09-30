d6a1e3cc9ac6e5400d74413f08ede416c1aa681a
Titouaaaan <titouanguerin@gmail.com>
2025-09-30 10:28:50 +0200

    updated question + bad code lul

--- DEEP-L/student_tp1/src/tp1.py
+++ DEEP-L/student_tp1/src/tp1.py
@@ -25,17 +25,47 @@ class MSE(Function):
         ## Garde les valeurs nécessaires pour le backwards
         ctx.save_for_backward(yhat, y)
 
-        #  TODO:  Renvoyer la valeur de la fonction
+        return 1/yhat.shape[0] * ((yhat - y) ** 2) 
 
     @staticmethod
     def backward(ctx, grad_output):
         ## Calcul du gradient du module par rapport a chaque groupe d'entrées
         yhat, y = ctx.saved_tensors
-        #  TODO:  Renvoyer par les deux dérivées partielles (par rapport à yhat et à y)
-
+        gradyhat = 2/yhat.shape[0] * (yhat - y) 
+        grady = -2/yhat.shape[0] * (yhat - y)
+        return gradyhat, grady
+    
 #  TODO:  Implémenter la fonction Linear(X, W, b)sur le même modèle que MSE
+
+class Linear(Function): 
+    @staticmethod
+    def forward(ctx, X, W, b):
+        ctx.save_for_backward(X, W, b)
+        return X @ W + b
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        X, W, b = ctx.saved_tensors
+        grad_X = grad_output @ W.T
+        grad_W = grad_output @ X.T
+        grad_b = grad_output.sum(0)
+        return grad_X, grad_W, grad_b
 
 ## Utile dans ce TP que pour le script tp1_gradcheck
 mse = MSE.apply
 linear = Linear.apply
+
+# Les données supervisées
+x = torch.randn(50, 13)
+y = torch.randn(50, 3)
+
+# Les paramètres du modèle à optimiser
+w = torch.randn(13, 3)
+b = torch.randn(3)
+context = Context()
+
+forwardpass = Linear.forward(context, x, w, b)
+loss = MSE.forward(context, forwardpass, y)
+backwardpass = MSE.backward(context, None)
+gx, gw, gb = Linear.backward(context, backwardpass)
 
